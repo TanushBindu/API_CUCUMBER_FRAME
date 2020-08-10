80d12ae3bae20f46b0d6098e18a1592fe8494572
tanub <tanub@DESKTOP-KF9JJK6>
2020-08-10 09:43:49 +0530

    API CUCUMBER FRAME

---
+++ src/test/java/TY/Utils/EndPoint.java
@@ -0,0 +1,24 @@
+package TY.Utils;
+
+public enum EndPoint 
+{
+	list_of_user("/api/users?page=2"),
+	Single_user("/api/users/2"),
+	Single_resource("/api/unknown/2"),
+	List_of_resource("/api/unknown"),
+	Single_user_not_found("/api/users/23"),
+	Single_resource_not_found("/api/unknown/23"),
+	Delay_response("/api/users?delay=3");
+	
+	
+	private String end;
+	EndPoint(String end)
+	{
+		this.end=end;
+	}
+	
+	public String get_end()
+	{
+		return end;
+	}
+}
